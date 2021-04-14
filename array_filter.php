f03e7dc2fa261f0ad23b0e64125f49655f2077d2
Aruna <patilarunash@gmail.com>
2021-04-14 18:01:36 +0530

    Code added to function

--- array_filter.php
+++ array_filter.php
@@ -1,5 +1,16 @@
 <?php
 
 function array_filter($array) {
-	
+	if( empty($array) || count($array) <= 0 ) 
+		return;
+	else {
+		$arr = [];
+
+		foreach ($array as $key => $value) {
+			if( !empty($value) ) {
+				$arr[$key] = $value;
+			}
+		}
+		return $arr;
+	}
 }
